7f38f530d34103e7bf936b418630b0e886291346
sammy <wtpai1120@gmail.com>
2022-10-22 12:19:11 +0800

    修改HelloWorld.sol

--- helloworld/contracts/HelloWorld.sol
+++ helloworld/contracts/HelloWorld.sol
@@ -2,21 +2,11 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorld {
-    // string message = "Hello World"; 
-    // function setMessage(string memory _message) public {    //設定Message(帶新值給Message)
-    //     message = _message;
-    // }
-    // function getMessage() public view returns (string memory) {    //取得Message
-    //     return message;
-    // }
-
-    string private info;
-
-    function setInfo(string memory _info) public {
-        info = _info;
+    string message = "Hello World"; 
+    function setMessage(string memory _message) public {
+        message = _message;
     }
-    
-    function getInfo() public view returns (string memory) {
-        return info;
+    function getMessage() public view returns (string memory) {
+        return message;
     }
 }
