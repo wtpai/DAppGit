a21ae1f64bcb4b900e8c2bbcc359c4e92b3e526d
sammy <wtpai1120@gmail.com>
2022-10-28 09:04:23 +0800

    last upload

--- MyToken/src/index.js
+++ MyToken/src/index.js
@@ -47,22 +47,7 @@ const App = {
     }
 }
 
-//window.App = App;
-
 window.addEventListener("load", function () {
-    // if (window.ethereum) {
-    //     // use MetaMask's provider
-    //     App.web3 = new Web3(window.ethereum);
-    //     window.ethereum.enable(); // get permission to access accounts
-    // } else {
-    //     console.warn(
-    //         "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
-    //     );
-    //     App.web3 = new Web3(
-    //         new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
-    //     );
-    // }
-
     // 檢查瀏覽器的ethereum
     if (window.ethereum) {
         App.web3 = new Web3(window.ethereum);
